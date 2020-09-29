e47e86c0b67adbf4fd21ef24541f05f84e3dcea2
erikotakami <tik.mtkneu_217@ezweb.ne.jp>
2020-09-29 17:35:20 +0900

    Userモデルのバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, uniqueness: true,
+                    format: { with: /\A\S+@\S+\.\S+\z/, message: 'は@を含む必要があります' }
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])\w{6,}\z/.freeze
+  validates :password,
+            format: { with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' }
 end
