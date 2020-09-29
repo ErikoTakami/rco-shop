0db5856863bbf75d73f02bb2cbac07adcf91a217
erikotakami <tik.mtkneu_217@ezweb.ne.jp>
2020-09-29 20:44:57 +0900

    バリテーションの変更
    deviseでemailにuniqueness設定してあるため修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, uniqueness: true,
-                    format: { with: /\A\S+@\S+\.\S+\z/, message: 'は@を含む必要があります' }
+  validates :email, format: { with: /\A\S+@\S+\.\S+\z/, message: 'は@を含む必要があります' }
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])\w{6,}\z/.freeze
   validates :password,
             format: { with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' }
