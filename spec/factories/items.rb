5b6c9ab71271e35393186db2d9b95fbf4fcadffc
erikotakami <tik.mtkneu_217@ezweb.ne.jp>
2020-10-01 16:18:21 +0900

    itemモデルのFactoryBot作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    title { Faker::Lorem.word }
+    artist { Faker::Artist.name }
+    text { Faker::Lorem.sentence }
+    genre_id { Faker::Number.between(from: 2, to: 19) }
+    country { 'JP' }  
+    format_id { Faker::Number.between(from: 2, to: 12) }
+    status_id { Faker::Number.between(from: 2, to: 10) }
+    price { '3000' }
+    stock { '10' }
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/dummy_box-8.png'), filename: 'dummy_box-8.png')
+    end
   end
 end
