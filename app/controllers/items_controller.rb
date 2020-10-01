0e9b7e9e25b14dfd0bd6509b8e92b5982a47cc7f
erikotakami <tik.mtkneu_217@ezweb.ne.jp>
2020-10-01 12:39:26 +0900

    itemsコントローラーの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,33 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :admin_check, only: [:new, :create]
+
   def index
+    @items = Item.order("created_at DESC")
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:title, :artist, :text, :genre_id, :country, :format_id, :status_id, :price, :stock, :image)
+  end
+
+  def admin_check
+    unless current_user.admin?
+      redirect_to root_path
+    end
+  end
+
 end
