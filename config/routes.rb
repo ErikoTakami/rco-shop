53aa90a5932294b726683822175a65a8f431198f
erikotakami <tik.mtkneu_217@ezweb.ne.jp>
2020-10-14 21:12:00 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items
+  resources :carts, only: [:show] 
+  resources :items do
+  post '/add_item' => 'carts#add_item'
+  put '/update_item' => 'carts#update_item'
+  delete '/delete_item' => 'carts#delete_item'
+  end
 end
+
