07877f06ca6809fd56b47f033304e251e5f7b5d9
erikotakami <tik.mtkneu_217@ezweb.ne.jp>
2020-10-14 21:34:44 +0900

    ヘルパーメソッドの設定
    stock_array_makerメソッド：商品の在庫数をビューでプルダウン表示できるようにする
    cart_item_price_calcメソッド：各商品ごとの合計金額の計算

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,2 +1,20 @@
 module CartsHelper
+  def stock_array_maker(cart_item)
+    current_stock_array = []                    #配列を生成
+
+    stock = cart_item.item.stock
+    stock.times do |quantity|                   #stockの現在の在庫数まで、ループを回す。
+      if quantity < stock                       #quantityが在庫未満かどうか？
+        current_stock_array << quantity + 1     #quantityは0からスタートしているので、1足した数を入れる必要がある。
+      else
+        break                                   #ループを抜ける
+      end
+    end
+    current_stock_array # 戻り値
+  end
+
+  def cart_item_price_calc(cart_item)
+    cart_item.item.price * cart_item.quantity   #商品価格×数量 = 各商品ごとの合計金額
+  end
+
 end
