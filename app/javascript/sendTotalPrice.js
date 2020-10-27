08e97d63aa1745f556fb1d02f2c5bb54ab952846
erikotakami <tik.mtkneu_217@ezweb.ne.jp>
2020-10-27 16:52:37 +0900

    sendTotalPrice.jsの修正

--- app/javascript/sendTotalPrice.js
+++ app/javascript/sendTotalPrice.js
@@ -1,9 +1,9 @@
 function sendTotalPrice() {
   const totalPriceDom = document.getElementById("cart-total-price")
   const totalPrice = parseInt(totalPriceDom.innerHTML);
-  
+
   const renderDom = document.getElementById("charge-form");
-  const totalPriceObj = `<input value=${totalPrice}, type="hidden", name="total_price">`;
+  const totalPriceObj = `<input value=${totalPrice} type="hidden" name="total_price">`;
   renderDom.insertAdjacentHTML("beforeend", totalPriceObj);
 };
 window.addEventListener('load', sendTotalPrice);
