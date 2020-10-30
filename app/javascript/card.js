658101865d238f7b3c404c93680ea22c76247fae
erikotakami <tik.mtkneu_217@ezweb.ne.jp>
2020-10-31 00:11:16 +0900

    card.jsの修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -7,26 +7,27 @@ const pay = () => {
     const formData = new FormData(formResult);
 
     const card = {
-      number: formData.get("number"),
-      cvc: formData.get("cvc"), 
-      exp_month: formData.get("exp_month"),
-      exp_year: `20${formData.get("exp_year")}`
+      number: formData.get("item_order[number]"),
+      cvc: formData.get("item_order[cvc]"), 
+      exp_month: formData.get("item_order[exp_month]"),
+      exp_year: `20${formData.get("item_order[exp_year]")}`
     };
+
     Payjp.createToken(card, (status, response) => {
       if (status === 200) {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
-        const tokenObj = `<input value=${token} type="hidden" name="card_token">`;
+        const tokenObj = `<input value=${token} type="hidden" name="item_order[card_token]">`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
-
-        document.getElementById("card-number").removeAttribute("name");
-        document.getElementById("card-exp-month").removeAttribute("name");
-        document.getElementById("card-exp-year").removeAttribute("name");
-        document.getElementById("card-cvc").removeAttribute("name");
-
-        renderDom.submit();
-        renderDom.reset();
       }
+
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
+      document.getElementById("charge-form").reset();
     });
   });
 };
