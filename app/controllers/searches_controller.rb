74a9d3a4a1561ce7f99007175870122410aad8e5
erikotakami <tik.mtkneu_217@ezweb.ne.jp>
2020-11-11 13:00:43 +0900

    Update searches_controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,17 @@
 class SearchesController < ApplicationController
+  before_action :search
+  
+  def keyword
+  end
+
+  # def sort_order
+  # end
+
+  private
+
+  def search
+    @p = Item.ransack(params[:q])
+    @result = @p.result
+  end
+
 end
