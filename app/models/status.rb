529ec036a4c06c23e0eea2bdc5ac72ce3ca83a32
erikotakami <tik.mtkneu_217@ezweb.ne.jp>
2020-11-13 14:30:45 +0900

    ActiveHashを修正

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,14 +1,13 @@
 class Status < ActiveHash::Base
   self.data = [
-    { id: 1, name: '--' },
-    { id: 2, name: 'S 未開封' },
-    { id: 3, name: 'A 無傷' },
-    { id: 4, name: 'A ほぼ無傷' },
-    { id: 5, name: 'B ほぼ美品状態' },
-    { id: 6, name: 'B ノイズほとんどなし' },
-    { id: 7, name: 'B 多少の汚れあり' },
-    { id: 8, name: 'B ノイズはあるが十分聴ける' },
-    { id: 9, name: 'B ノイズがある' },
-    { id: 10, name: 'C かなりノイズがある' }
+    { id: 1, name: 'S 未開封' },
+    { id: 2, name: 'A 無傷' },
+    { id: 3, name: 'A ほぼ無傷' },
+    { id: 4, name: 'B ほぼ美品状態' },
+    { id: 5, name: 'B ノイズほとんどなし' },
+    { id: 6, name: 'B 多少の汚れあり' },
+    { id: 7, name: 'B ノイズはあるが十分聴ける' },
+    { id: 8, name: 'B ノイズがある' },
+    { id: 9, name: 'C かなりノイズがある' }
   ]
 end
